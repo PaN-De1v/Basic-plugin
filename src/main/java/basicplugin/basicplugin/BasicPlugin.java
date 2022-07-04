417cb6221bbb80026403acad0f9e4e99eb7cf573
De1v <dejvklima@gmail.com>
2022-07-04 09:46:06 +0200

    Implemented plugin

---
+++ src/main/java/basicplugin/basicplugin/BasicPlugin.java
@@ -0,0 +1,17 @@
+package basicplugin.basicplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BasicPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
